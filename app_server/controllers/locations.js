977aecb9ede90438930b3247da613487dd505db2
Noah-Huff <noah.b.huff@gmail.com>
2020-10-31 17:01:26 -0500

    " "

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,18 @@
+/* get home page*/
+const homelist = (req, res) => {
+    res.render('index', { title: 'Home'});
+};
+/* get locations info page*/
+const locationInfo = (req, res) => {
+    res.render('index', { title: 'Location Info'});
+};
+/* get add review page*/
+const addReview = (req, res) => {
+    res.render('index', { title: 'Add Review'});
+};
+
+module.exports = {
+    homelist,
+    locationInfo,
+    addReview
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main');
+//const ctrlMain = require('../controllers/main'); // I don't think I need this.... not for sure yet though
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
+
+// Locations Pages
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+//others page
+router.get('/about', ctrlOthers.about);
+
 
 /* GET home page. 
 router.get('/', function(req, res, next) {
@@ -11,6 +21,6 @@ const homepageController = (req, res) => {
   res.render('index', { title: 'Express'});
 };
 /* GET home page*/
-router.get('/', ctrlMain.index);
+//router.get('/', ctrlMain.index);
 
 module.exports = router;
