4459ec9a4a5bc9426ce5833baf03bf099348d326
Noah-Huff <noah.b.huff@gmail.com>
2021-01-14 18:42:21 -0600

    passing lat, lng to map in location-details ts

--- app_public/src/app/location-details/location-details.component.ts
+++ app_public/src/app/location-details/location-details.component.ts
@@ -43,7 +43,11 @@ export class LocationDetailsComponent implements OnInit {
 
     console.log('LOCATION-DETAILS', location);
 */
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+var lat = this.location.coords.coordinates[1];
+var lng = this.location.coords.coordinates[0];
+
+var mymap = L.map('mapid').setView([lat, lng], 13);
 
 L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
